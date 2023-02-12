93cec7d72cca74e4cc22eca2ff6aa3f87cf9a00e
AgusB <agustinpbosio@outlook.com.ar>
2023-02-12 19:20:33 -0300

    trabajo casi terminado

--- js/actualizarpagina.js
+++
@@ -1,3 +0,0 @@
-function actualizarPagina() {
-    location.reload();
-  }
--- js/obtener.datos.js
+++ js/obtener.datos.js
@@ -1,24 +1,27 @@
 fetch("https://randomuser.me/api/?nat=ES")
   .then(response => response.json())
   .then(json => {
-    // Aquí puede acceder a los datos de la respuesta JSON
     console.log(json);
 
     var nombre = json.results[0].name.first + " " + json.results[0].name.last;
     var edad = json.results[0].dob.age;
     var telefono = json.results[0].phone;
-    var email = json.results[0].email;
+    var correo = json.results[0].email;
     var foto = json.results[0].picture.medium;
     var calle = json.results[0].location.street.name;
     var ciudad = json.results[0].location.city;
-    var codigoPostal = json.results[0].location.postcode;
+    var codigopostal = json.results[0].location.postcode;
     var nacionalidad = json.results[0].nat;
 
-
-
-document.getElementById("nombre").innerHTML = nombre;
-
-
+    document.getElementsByClassName("foto")[0].innerHTML = '<img src="' + foto + '">';
+    document.getElementById("nombre").innerHTML = nombre;
+    document.getElementById("correo").innerHTML = correo;
+    document.getElementById("edad").innerHTML = edad;
+    document.getElementById("telefono").innerHTML = telefono;
+    document.getElementById("calle").innerHTML = calle;
+    document.getElementById("ciudad").innerHTML = ciudad;
+    document.getElementById("codigopostal").innerHTML = codigopostal;
+    document.getElementById("nacionalidad").innerHTML = nacionalidad;
 
   });
 
